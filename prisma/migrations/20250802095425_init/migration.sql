ab2ac643f83e4f4671f9905284b34d72459433a6
Bruno Lima <brunojet@gmail.com>
2025-08-03 05:46:59 -0300

    Start.

--- prisma/migrations/20250802095425_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the `auditoria_catalogo` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "store_nodejs"."auditoria_catalogo";
--- prisma/migrations/20250802104551_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "versao_aplicativo_configuracao_aplicativo_id_idx" ON "store_nodejs"."versao_aplicativo"("configuracao_aplicativo_id");

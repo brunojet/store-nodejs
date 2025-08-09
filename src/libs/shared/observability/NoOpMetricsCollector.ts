21df5340799ce34f51a2d880b0e99f82a75e80fd
Bruno Lima <brunojet@gmail.com>
2025-08-09 18:56:38 -0300

    Clean again!

--- src/libs/shared/observability/NoOpMetricsCollector.ts
+++
@@ -1,8 +0,0 @@
-import type {MetricsCollector} from './index.js';
-
-export class NoOpMetricsCollector implements MetricsCollector {
-  counter(): void {}
-  gauge(): void {}
-  histogram(): void {}
-  timing(): void {}
-}

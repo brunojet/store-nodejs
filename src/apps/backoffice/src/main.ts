d595602b1e233529919e59ead11b3c7ce1f97f75
Bruno Lima <brunojet@gmail.com>
2025-08-09 21:53:28 -0300

    RUN!

--- src/apps/backoffice/src/main.ts
+++ src/apps/backoffice/src/main.ts
@@ -1,14 +1,22 @@
-import express from 'express';
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import express from 'express';
+import * as path from 'path';
+import { shared } from '@shared';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.send({ message: shared() });
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
 });
+server.on('error', console.error);
